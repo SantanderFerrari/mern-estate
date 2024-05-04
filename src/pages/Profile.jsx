6bf7f88b74885b3c984a0ac572a32c3ddaf21db5
Daniel Meshack Wako <DANIEL.WAKO98@GMAIL.COM>
2024-05-04 19:48:15 +0300

    complete show user listings functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -16,6 +16,8 @@ export default function Profile() {
         (false);
     const [formData, setFormData] = useState({});
     const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
 
     //firebase storage
@@ -120,6 +122,21 @@ export default function Profile() {
         }
     };
 
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success == false) {
+                setShowListingsError(true);
+                return;
+            }
+
+            setUserListings(data);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    }
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3x1 font-semibold 
@@ -200,7 +217,54 @@ export default function Profile() {
             <p className='text-green-700 mt-5'>
                 {updateSuccess ? 'User is  updated Successfully' : ''}
             </p>
+            <button
+                onClick={handleShowListings}
+                className='text-green-800 w-full'>Show listings</button>
+            <p
+                className='text-red-900 mt-5'
+            >
+                {showListingsError ? 'Error showing listings' : ''}
+            </p>
 
+            {userListings && userListings.length > 0 && (
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-center mt-7 text-2xl font-semibold'>
+                        Your Listings
+                    </h1>
+                    {userListings.map((listing) => (
+                        <div
+                            key={listing._id}
+                            className='border rounded-lg p-3 flex justify-between items-center gap-4'
+                        >
+                            <Link to={`/listing/${listing._id}`}>
+                                <img
+                                    src={listing.imageURLs[0]}
+                                    alt='listing cover'
+                                    className='h-16 w-16 object-contain'
+                                />
+                            </Link>
+                            <Link
+                                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                                to={`/listing/${listing._id}`}
+                            >
+                                <p>{listing.name}</p>
+                            </Link>
+
+                            <div className='flex flex-col item-center'>
+                                <button
+                                    onClick={() => handleListingDelete(listing._id)}
+                                    className='text-red-700 uppercase'
+                                >
+                                    Delete
+                                </button>
+                                <Link to={`/update-listing/${listing._id}`}>
+                                    <button className='text-green-700 uppercase'>Edit</button>
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-    )
+    );
 }
